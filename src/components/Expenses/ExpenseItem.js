be62513edc811f9925ca6b80e7ca3e376ebbabaa
shivrajpro <shivrajhulbatte@gmail.com>
2023-01-12 15:27:08 +0530

    adding Conditional Return Statements

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,4 +1,4 @@
-import Card from '../UI/Card';
+import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
@@ -6,13 +6,15 @@ function ExpenseItem(props) {
   const expenseAmount = props.amount;
 
   return (
-    <Card className="expense-item">
-      <ExpenseDate date={props.date} />
-      <div className="expense-item__description">
-        <h2> {props.title} </h2>
-        <div className="expense-item__price">${expenseAmount} </div>
-      </div>
-    </Card>
+    <li>
+      <Card className="expense-item">
+        <ExpenseDate date={props.date} />
+        <div className="expense-item__description">
+          <h2> {props.title} </h2>
+          <div className="expense-item__price">${expenseAmount} </div>
+        </div>
+      </Card>
+    </li>
   );
 }
 
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,33 +1,20 @@
-import ExpenseItem from "./ExpenseItem";
 import ExpensesFilter from "./ExpensesFilter";
 import Card from "../UI/Card";
 import { useState } from "react";
 import "./Expenses.css";
+import ExpensesList from "./ExpensesList";
 
 function Expenses(props) {
   const [filteredYear, setFilteredYear] = useState("2020");
 
   const yearChangeHandler = (year) => {
-    console.log("SELECTED YEAR", year, filteredExpenses);
+    // console.log("SELECTED YEAR", year, filteredExpenses);
     setFilteredYear(year);
   };
 
   const filteredExpenses = props.expenses.filter(exp=>{
     return exp.date.getFullYear() === +filteredYear;
   }) 
-
-  let expensesContent = <p>No expenses found</p>;
-
-  if(filteredExpenses.length){
-    expensesContent = filteredExpenses.map((expense) => (
-      <ExpenseItem
-        key={expense.id}
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-      />
-    ))
-  }
 
   return (
     <div>
@@ -36,7 +23,7 @@ function Expenses(props) {
           selectedYear={filteredYear}
           onYearSelect={yearChangeHandler}
         />
-        {expensesContent}
+        <ExpensesList expenses={filteredExpenses}/>
       </Card>
     </div>
   );
