747f3cf78a21b6939b0f8322fce012899730d1b1
shivrajpro <shivrajhulbatte@gmail.com>
2023-01-18 15:19:43 +0530

    creating a wrapper cmp

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import Wrapper from "./components/Helpers/Wrapper";
 
 import AddUser from "./components/Users/AddUser";
 import UsersList from "./components/Users/UsersList";
@@ -16,10 +17,10 @@ function App() {
   };
 
   return (
-    <div>
+    <Wrapper>
       <AddUser onAddUser={addUserHandler} />
       <UsersList users={usersList} />
-    </div>
+    </Wrapper>
   );
 }
 
