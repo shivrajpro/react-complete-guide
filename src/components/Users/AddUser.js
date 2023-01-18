98ba2049a96020f40b99d6fd3ea53d6caf66d33e
shivrajpro <shivrajhulbatte@gmail.com>
2023-01-18 13:32:24 +0530

    Managing a List Of Users via State

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -10,13 +10,13 @@ const AddUser = (props) => {
 
   const addUserHandler = (event) => {
     event.preventDefault();
-    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+    if (!enteredUsername.trim().length || !enteredAge.trim().length) {
       return;
     }
     if (+enteredAge < 1) {
       return;
     }
-    console.log(enteredUsername, enteredAge);
+    props.onAddUser(enteredUsername, enteredAge);
     setEnteredUsername('');
     setEnteredAge('');
   };
