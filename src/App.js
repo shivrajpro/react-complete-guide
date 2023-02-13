f0c49f7ee14ccf15703d25ecbe7a94f3a44ac55d
shivrajpro <shivrajhulbatte@gmail.com>
2023-02-13 16:49:18 +0530

    example of class based components

--- src/App.js
+++ src/App.js
@@ -1,29 +1,10 @@
-import { useState } from 'react';
-
-import Cart from './components/Cart/Cart';
-import Header from './components/Layout/Header';
-import Meals from './components/Meals/Meals';
-import CartProvider from './store/CartProvider';
+import Users from './components/Users';
 
 function App() {
-  const [cartIsShown, setCartIsShown] = useState(false);
-
-  const showCartHandler = () => {
-    setCartIsShown(true);
-  };
-
-  const hideCartHandler = () => {
-    setCartIsShown(false);
-  };
-
   return (
-    <CartProvider>
-      {cartIsShown && <Cart onClose={hideCartHandler} />}
-      <Header onShowCart={showCartHandler} />
-      <main>
-        <Meals />
-      </main>
-    </CartProvider>
+    <div>
+      <Users />
+    </div>
   );
 }
 
