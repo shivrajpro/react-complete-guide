68d815f5b753f719334752954050559d583f069e
shivrajpro <shivrajhulbatte@gmail.com>
2023-02-15 17:50:56 +0530

    using useCallback  hook to manage the dependency in useEffect hook in app.js for fetchTasks

--- src/App.js
+++ src/App.js
@@ -1,29 +1,37 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
-import Tasks from './components/Tasks/Tasks';
-import NewTask from './components/NewTask/NewTask';
-import useHttp from './hooks/use-http';
+import Tasks from "./components/Tasks/Tasks";
+import NewTask from "./components/NewTask/NewTask";
+import useHttp from "./hooks/use-http";
 
 function App() {
   const [tasks, setTasks] = useState([]);
-  
-  const transformTasks = (tasksObj) =>{
+
+  const { isLoading, error, sendRequest: fetchTasks } = useHttp();
+
+  useEffect(() => {
+    const transformTasks = (tasksObj) => {
       const loadedTasks = [];
 
       for (const taskKey in tasksObj) {
         loadedTasks.push({ id: taskKey, text: tasksObj[taskKey].text });
       }
 
-      setTasks(loadedTasks);    
-  }
+      setTasks(loadedTasks);
+    };
+  
+    fetchTasks(
+      { url: "https://react-http-f2187-default-rtdb.firebaseio.com/tasks.json"},
+      transformTasks
+    );
 
-  const { isLoading, error, sendRequest:fetchTasks } = useHttp({
-    url:'https://react-http-f2187-default-rtdb.firebaseio.com/tasks.json'
-  }, transformTasks);
-
-  useEffect(() => {
-    fetchTasks();
-  }, []);
+  }, [fetchTasks]);
+  // If we specify only fetchTasks reference here in the dependency
+  // it will lead to an inifinite loop of API calls because the reference of fetchTasks
+  // keeps on changing after every request as its an async function
+  // to avoid this - we must wrap the async function in useHttp hook
+  // in useCallback hook which returns the same reference after an API call
+  // and this useEffect hoook does not go in infinite loop
 
   const taskAddHandler = (task) => {
     setTasks((prevTasks) => prevTasks.concat(task));
--- src/hooks/use-http.js
+++ src/hooks/use-http.js
@@ -1,30 +1,31 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 
-const useHttp = (requestConfig, applyData)=>{
+const useHttp = ()=>{
     const [isLoading, setIsLoading] = useState(false);
     const [error, setError] = useState(null);
   
-    const sendRequest = async () => {
+    const sendRequest = useCallback(async (requestConfig, applyData) => {
       setIsLoading(true);
       setError(null);
       try {
         const response = await fetch(requestConfig.url, {
-            method: requestConfig.method || 'GET',
-            headers: requestConfig.headers || {},
-            body: requestConfig.body ? JSON.stringify(requestConfig.body) : null 
+          method: requestConfig.method || "GET",
+          headers: requestConfig.headers || {},
+          body: requestConfig.body ? JSON.stringify(requestConfig.body) : null,
         });
-  
+
         if (!response.ok) {
-          throw new Error('Request failed!');
+          throw new Error("Request failed!");
         }
-  
+
         const data = await response.json();
         applyData(data);
       } catch (err) {
-        setError(err.message || 'Something went wrong!');
+        setError(err.message || "Something went wrong!");
       }
       setIsLoading(false);
-    };
+    }, []);
+
 
     return { isLoading, error, sendRequest };
 }
