bf17f60cfd1bdb0ab4154e2b34c969afce550477
shivrajpro <shivrajhulbatte@gmail.com>
2023-02-16 19:55:37 +0530

    adding handler in simple input

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,9 +1,32 @@
+import { useRef } from "react";
+import { useState } from "react";
+
 const SimpleInput = (props) => {
+  const [enteredName, setEnteredName] = useState('');
+  const nameInputRef = useRef();
+
+  const nameInputChangeHandler = (evt)=>{
+    setEnteredName(evt.target.value);
+  }
+
+  const formSubmitHandler = (evt)=>{
+    evt.preventDefault();
+
+    console.log('name=',enteredName);
+
+    setEnteredName('');
+  }
+
   return (
-    <form>
+    <form onSubmit={formSubmitHandler}>
       <div className='form-control'>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+        <input ref={nameInputRef} 
+        type='text' 
+        id='name' 
+        onChange={nameInputChangeHandler}
+        value={enteredName}
+        />
       </div>
       <div className="form-actions">
         <button>Submit</button>
