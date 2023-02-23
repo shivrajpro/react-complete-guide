1dbab31582405e3bd13c569be582e5da9c68efbb
shivrajpro <shivrajhulbatte@gmail.com>
2023-02-23 15:28:17 +0530

    useDispatch and useSelector demo

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,28 @@
+import { useDispatch, useSelector } from 'react-redux';
 import classes from './Counter.module.css';
 
 const Counter = () => {
+  const counter = useSelector(state=>state.counter);
+  const dispatch = useDispatch();
+
+  const onIncrement = ()=>{
+    dispatch({type:'increment'});
+  }
+ 
+  const onDecrement = ()=>{
+    dispatch({type:'decrement'});
+  }
+
   const toggleCounterHandler = () => {};
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{counter}</div>
+      <div className='counter'>
+        <button onClick={onIncrement} >Increment</button>
+        <button onClick={onDecrement} >Decrement</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,6 @@
 import { createStore } from "redux";
 
-const counterReducer = (state, action) => {
+const counterReducer = (state = {counter :0}, action) => {
   if (action.type === "increment") return { counter: state.counter + 1 };
   if (action.type === "decrement") return { counter: state.counter - 1 };
   return state;
