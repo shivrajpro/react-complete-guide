9118cdc57e6019a1d027d05fb23b0b0c082f61fa
shivrajpro <shivrajhulbatte@gmail.com>
2023-02-24 13:45:40 +0530

    fix login bug

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -5,14 +5,15 @@ import classes from './Auth.module.css';
 const Auth = () => {
   const dispatch = useDispatch();
   
-  const onLogin = ()=>{
+  const onLogin = (evt)=>{
+    evt.preventDefault();
     dispatch(authActions.login());
   }
   
   return (
     <main className={classes.auth}>
       <section>
-        <form>
+        <form onSubmit={onLogin}>
           <div className={classes.control}>
             <label htmlFor='email'>Email</label>
             <input type='email' id='email' />
@@ -21,7 +22,7 @@ const Auth = () => {
             <label htmlFor='password'>Password</label>
             <input type='password' id='password' />
           </div>
-          <button onClick={onLogin} >Login</button>
+          <button>Login</button>
         </form>
       </section>
     </main>
