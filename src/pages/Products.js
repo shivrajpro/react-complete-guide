704f8cb490b1a414469041e0c072d1eefb35c2b8
shivrajpro <shivrajhulbatte@gmail.com>
2023-02-27 14:08:34 +0530

    route: navigate programmatically

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,9 +1,15 @@
 import { Fragment } from "react";
-
+import { useNavigate } from "react-router-dom";
 const ProductsPage = () => {
+  const navigate = useNavigate();
+
+  const goToHome = ()=>{
+    navigate('/');
+  }
   return (
     <Fragment>
       <h1>My Products Page</h1>
+      <button onClick={goToHome} >Home</button>
       {/* <Link to="../">Back</Link> */}
     </Fragment>
   );
