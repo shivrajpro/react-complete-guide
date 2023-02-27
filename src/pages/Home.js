0a82890a522f90ea76c121e2862f83117ad81eca
shivrajpro <shivrajhulbatte@gmail.com>
2023-02-27 12:23:22 +0530

    routing: adding home and products routes

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import { Fragment } from "react";
+
+const HomePage = () => {
+  return (
+    <Fragment>
+      <h1>My Home Page</h1>
+      <a href="products" >Products</a>
+    </Fragment>
+  );
+};
+
+export default HomePage;
---
+++ src/pages/Products.js
@@ -0,0 +1,5 @@
+const ProductsPage = ()=>{
+    return <h1>This is products page</h1>
+}
+
+export default ProductsPage;
