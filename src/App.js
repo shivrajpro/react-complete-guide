46446ddcc5594e53334697152e10f93e02136b51
shivrajpro <shivrajhulbatte@gmail.com>
2023-02-27 13:20:57 +0530

    routing: adding error element

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {
   // Route,
 } from 'react-router-dom';
 
+import ErrorPage from './pages/Error';
 import HomePage from './pages/Home';
 import ProductsPage from './pages/Products';
 import RootLayout from './pages/Root';
@@ -20,6 +21,7 @@ const router = createBrowserRouter([
   {
     path: '/',
     element: <RootLayout />,
+    errorElement: <ErrorPage />,
     children: [
       { path: '/', element: <HomePage /> },
       { path: '/products', element: <ProductsPage /> },
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,11 @@
 import { Fragment } from "react";
+import { Link } from "react-router-dom";
+
 const HomePage = () => {
   return (
     <Fragment>
       <h1>My Home Page</h1>
+      <Link to="/abc" >ABC</Link>
     </Fragment>
   );
 };
