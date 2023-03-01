e087580774c563ceea9e4d45bf4595d5adafa6eb
shivrajpro <shivrajhulbatte@gmail.com>
2023-03-01 12:54:35 +0530

    error handling in routes

--- src/pages/Events.js
+++ src/pages/Events.js
@@ -1,9 +1,14 @@
-import { useLoaderData } from 'react-router-dom';
+import { useLoaderData, json } from "react-router-dom";
 
-import EventsList from '../components/EventsList';
+import EventsList from "../components/EventsList";
 
 function EventsPage() {
-  const events = useLoaderData();
+  const data = useLoaderData();
+
+  // if (data.isError) {
+  //   return <p>{data.message}</p>;
+  // }
+  const events = data.events;
 
   return <EventsList events={events} />;
 }
@@ -11,12 +16,16 @@ function EventsPage() {
 export default EventsPage;
 
 export async function loader() {
-  const response = await fetch('http://localhost:8080/events');
+  const response = await fetch("http://localhost:8080/events");
 
   if (!response.ok) {
-    // ...
-  } else {
-    const resData = await response.json();
-    return resData.events;
+    // return { isError: true, message: 'Could not fetch events.' };
+    // throw new Response(JSON.stringify({ message: 'Could not fetch events.' }), {
+    //   status: 500,
+    // });
+    throw json({ message: "Could not fetch events." }, { status: 500 });
+
   }
+
+  return response;
 }
