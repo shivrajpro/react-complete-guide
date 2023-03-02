fc2fca3fb11b371772cff11e7cb04becfff511e9
shivrajpro <shivrajhulbatte@gmail.com>
2023-03-02 12:15:40 +0530

    working with edit event form using a common action

--- src/App.js
+++ src/App.js
@@ -1,52 +1,27 @@
-// Challenge / Exercise
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
-// 1. Add five new (dummy) page components (content can be simple <h1> elements)
-//    - HomePage
-//    - EventsPage
-//    - EventDetailPage
-//    - NewEventPage
-//    - EditEventPage
-// DONE
-// 2. Add routing & route definitions for these five pages
-//    - / => HomePage
-//    - /events => EventsPage
-//    - /events/<some-id> => EventDetailPage
-//    - /events/new => NewEventPage
-//    - /events/<some-id>/edit => EditEventPage
-// DONE
-// 3. Add a root layout that adds the <MainNavigation> component above all page components
-// DONE
-// 4. Add properly working links to the MainNavigation
-// DONE
-// 5. Ensure that the links in MainNavigation receive an "active" class when active
-// DONE
-// 6. Output a list of dummy events to the EventsPage
-//    Every list item should include a link to the respective EventDetailPage
-// DONE
-// 7. Output the ID of the selected event on the EventDetailPage
-// DONE
-// BONUS: Add another (nested) layout route that adds the <EventNavigation> component above all /events... page components
-// DONE
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-
-import EditEventPage from "./pages/EditEvent";
-import EventDetailPage, { action as deleteEventAction, loader as eventDetailLoader} from "./pages/EventDetail";
-import EventsPage, { loader as eventsLoader } from "./pages/Events";
-import EventsRootLayout from "./pages/EventsRoot";
-import HomePage from "./pages/Home";
-import NewEventPage, { action as newEventAction } from "./pages/NewEvent";
-import RootLayout from "./pages/Root";
-import ErrorPage from "./pages/Error";
+import EditEventPage from './pages/EditEvent';
+import ErrorPage from './pages/Error';
+import EventDetailPage, {
+  loader as eventDetailLoader,
+  action as deleteEventAction,
+} from './pages/EventDetail';
+import EventsPage, { loader as eventsLoader } from './pages/Events';
+import EventsRootLayout from './pages/EventsRoot';
+import HomePage from './pages/Home';
+import NewEventPage from './pages/NewEvent';
+import RootLayout from './pages/Root';
+import { action as manipulateEventAction } from './components/EventForm';
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: <RootLayout />,
     errorElement: <ErrorPage />,
     children: [
       { index: true, element: <HomePage /> },
       {
-        path: "events",
+        path: 'events',
         element: <EventsRootLayout />,
         children: [
           {
@@ -54,23 +29,32 @@ const router = createBrowserRouter([
             element: <EventsPage />,
             loader: eventsLoader,
           },
-          { path: ":eventId",
-            loader:eventDetailLoader,
-            id:'event-detail',
-            children:[
+          {
+            path: ':eventId',
+            id: 'event-detail',
+            loader: eventDetailLoader,
+            children: [
               {
-                index:true,
+                index: true,
                 element: <EventDetailPage />,
-                action: deleteEventAction                
+                action: deleteEventAction,
               },
-              { path: "edit", element: <EditEventPage /> }
-            ]
+              {
+                path: 'edit',
+                element: <EditEventPage />,
+                action: manipulateEventAction,
+              },
+            ],
           },
-          { path: "new", element: <NewEventPage />, action: newEventAction }
-        ]
-      }
-    ]
-  }
+          {
+            path: 'new',
+            element: <NewEventPage />,
+            action: manipulateEventAction,
+          },
+        ],
+      },
+    ],
+  },
 ]);
 
 function App() {
--- src/components/EventForm.js
+++ src/components/EventForm.js
@@ -1,19 +1,34 @@
-import { Form, useNavigate, useNavigation } from "react-router-dom";
+import {
+  Form,
+  useNavigate,
+  useNavigation,
+  useActionData,
+  json,
+  redirect
+} from 'react-router-dom';
 
-import classes from "./EventForm.module.css";
+import classes from './EventForm.module.css';
 
-function EventForm({ method, event }) {
+function EventForm({ method, eventData }) {
+  const data = useActionData();
   const navigate = useNavigate();
   const navigation = useNavigation();
 
-  const isSubmitting = navigation.state === "submitting";
+  const isSubmitting = navigation.state === 'submitting';
 
   function cancelHandler() {
-    navigate("..");
+    navigate('..');
   }
 
   return (
-    <Form method="post" className={classes.form}>
+    <Form method={method} className={classes.form}>
+      {data && data.errors && (
+        <ul>
+          {Object.values(data.errors).map((err) => (
+            <li key={err}>{err}</li>
+          ))}
+        </ul>
+      )}
       <p>
         <label htmlFor="title">Title</label>
         <input
@@ -21,7 +36,7 @@ function EventForm({ method, event }) {
           type="text"
           name="title"
           required
-          defaultValue={event ? event.title : ""}
+          defaultValue={eventData ? eventData.title : ''}
         />
       </p>
       <p>
@@ -31,7 +46,7 @@ function EventForm({ method, event }) {
           type="url"
           name="image"
           required
-          defaultValue={event ? event.image : ""}
+          defaultValue={eventData ? eventData.image : ''}
         />
       </p>
       <p>
@@ -41,7 +56,7 @@ function EventForm({ method, event }) {
           type="date"
           name="date"
           required
-          defaultValue={event ? event.date : ""}
+          defaultValue={eventData ? eventData.date : ''}
         />
       </p>
       <p>
@@ -51,7 +66,7 @@ function EventForm({ method, event }) {
           name="description"
           rows="5"
           required
-          defaultValue={event ? event.description : ""}
+          defaultValue={eventData ? eventData.description : ''}
         />
       </p>
       <div className={classes.actions}>
@@ -59,7 +74,7 @@ function EventForm({ method, event }) {
           Cancel
         </button>
         <button disabled={isSubmitting}>
-          {isSubmitting ? "Submitting..." : "Save"}
+          {isSubmitting ? 'Submitting...' : 'Save'}
         </button>
       </div>
     </Form>
@@ -67,3 +82,41 @@ function EventForm({ method, event }) {
 }
 
 export default EventForm;
+
+export async function action({ request, params }) {
+  const method = request.method;
+  const data = await request.formData();
+
+  const eventData = {
+    title: data.get('title'),
+    image: data.get('image'),
+    date: data.get('date'),
+    description: data.get('description'),
+  };
+
+  let url = 'http://localhost:8080/events';
+
+  if (method === 'PATCH') {
+    const eventId = params.eventId;
+    url = 'http://localhost:8080/events/' + eventId;
+  }
+
+  const response = await fetch(url, {
+    method: method,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(eventData),
+  });
+
+  if (response.status === 422) {
+    return response;
+  }
+
+  if (!response.ok) {
+    throw json({ message: 'Could not save event.' }, { status: 500 });
+  }
+
+  return redirect('/events');
+}
+
