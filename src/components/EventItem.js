e4469567d4255ef3e03d0a6ffba2933cab70497e
shivrajpro <shivrajhulbatte@gmail.com>
2023-03-03 15:44:53 +0530

    auth: adding auto logout

--- src/components/EventItem.js
+++ src/components/EventItem.js
@@ -1,16 +1,16 @@
-import { Link, useSubmit } from 'react-router-dom';
+import { Link, useRouteLoaderData, useSubmit } from "react-router-dom";
 
-import classes from './EventItem.module.css';
+import classes from "./EventItem.module.css";
 
 function EventItem({ event }) {
+  const token = useRouteLoaderData("root");
   const submit = useSubmit();
 
   function startDeleteHandler() {
-    const proceed = window.confirm('Are you sure?');
+    const proceed = window.confirm("Are you sure?");
 
     if (proceed) {
-      submit(null, { method: 'delete' });// request will go to an action
-      // written in event detail
+      submit(null, { method: "delete" });
     }
   }
 
@@ -20,10 +20,12 @@ function EventItem({ event }) {
       <h1>{event.title}</h1>
       <time>{event.date}</time>
       <p>{event.description}</p>
-      <menu className={classes.actions}>
-        <Link to="edit">Edit</Link>
-        <button onClick={startDeleteHandler}>Delete</button>
-      </menu>
+      {token && (
+        <menu className={classes.actions}>
+          <Link to="edit">Edit</Link>
+          <button onClick={startDeleteHandler}>Delete</button>
+        </menu>
+      )}
     </article>
   );
 }
